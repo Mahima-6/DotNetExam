faa1d97506634a4abcacf3e945919912dc8ba107
Mahima Modhawala <89176914+Mahima-6@users.noreply.github.com>
2022-02-19 18:04:54 +0530

    Add files via upload

---
+++ Products.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MahimaModhawala.Models
+{
+    public class Products
+    {
+        
+
+        public int ProductId { get; set; }
+        
+        [Required(ErrorMessage="please enter Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "please enter Rate")]
+
+        public decimal Rate { get; set; }
+
+        [Required(ErrorMessage = "please enter Description")]
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "please enter CategoryName")]
+
+        public string CategoryName { get; set; }
+    }
+}
